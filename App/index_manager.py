db0bc6b7c1a53511afbe9d92471c513d6a46db58
rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2024-05-31 12:14:26 +0200

    refactoring index manager

--- App/index_manager.py
+++ App/index_manager.py
@@ -1,57 +1,79 @@
+import sys
 from txtai.embeddings import Embeddings
 import os
 
 class IndexManager:
     def __init__(self):
         self.embeddings = Embeddings(
-        content=True,
-        defaults=False,
-        indexes={
-            "column_description": {
-                "path": "sentence-transformers/all-MiniLM-L12-v2"
-            },
-             "column_description_multilingual": {
-                "path": "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-            },
-            "table_description_with_column_name_and_synonyms": {
-                "path": "sentence-transformers/all-MiniLM-L12-v2",
-                "columns": {
-                    "text": "relevant_information"
+            content=True,
+            defaults=False,
+            indexes={
+                "column_description": {
+                    "path": "sentence-transformers/all-MiniLM-L12-v2"
+                },
+                "column_description_multilingual": {
+                    "path": "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+                },
+                "table_description_with_column_name_and_synonyms": {
+                    "path": "sentence-transformers/all-MiniLM-L12-v2",
+                    "columns": {
+                        "text": "intent"
+                    }
                 }
             }
-        }
-    )
+        )
+        self.path = "Indici"
+        
+    def setDataDictName(self, data_dict_name):
+        self.data_dict_name = data_dict_name
 
     def createIndex(self, documents):
         self.embeddings.index(documents)
 
-    def getResult(self, user_request):
+    def __getTuples(self, user_request):
         query_limit = 20
         sql_query = f"""
             SELECT table_name, MAX(score) AS max_score, AVG(score) AS avg_score
             FROM txtai WHERE 
             similar(':x', 'column_description') and
             similar(':x', 'column_description_multilingual') and
-            similar(':x', 'table_description_with_column_name_and_synonyms') and
             score >= 0.2
             GROUP BY table_name HAVING
             avg_score >= 0.25
             ORDER BY max_score DESC
             LIMIT {query_limit}
         """
-        relevant_tables = self.embeddings.search(sql_query, limit=query_limit*10, parameters={"x": user_request})
-        
-        return relevant_tables
+        #similar(':x', 'table_description_with_column_name_and_synonyms') and
+        tuples = self.embeddings.search(sql_query, limit=query_limit*10, parameters={"x": user_request})
+        relevant_tuples = self.__getRelevantTuples(tuples)
+        return relevant_tuples
+
+    def __getRelevantTuples(self, tuples):
+        #[f"'{table['table_name']}'" for table in results]
+        i = 0
+        score = 0
+        relevant_tuples = []
+        for tuple in enumerate(tuples):
+            print(score - int(tuple[1]['max_score']))
+            if (score - int(tuple[1]['max_score'])) <= 0.3:
+                relevant_tuples.append([tuple[1]['table_name'], tuple[1]['max_score']])
+                score = tuple[1]['max_score']
+                print(score)
+            else:
+                break
+        return relevant_tuples
     
     def saveIndex(self):
-        self.embeddings.save("idx")
+        self.embeddings.save(f"{self.path}/{self.data_dict_name}")
     
     def loadIndex(self):
-        self.embeddings.load("idx")
+        #self.embeddings.load(f"{self.path}/{self.data_dict_name}")
+        self.embeddings.load(f"Indici/idx")
 
     def createOrLoadIndex(self, documents):
         createIDX = True
-        if "idx" in next(os.walk(os.getcwd())):
+        path = f"{self.path}/{self.data_dict_name}"
+        if os.path.exists(path):
             createIDX = False
         if createIDX:
             self.createIndex(documents)
@@ -61,21 +83,25 @@ class IndexManager:
             self.loadIndex()
             return False
         
-    def printFormattedResult(self, result):
+    def promptGenerator(self, user_request):
+        relevant_tuples = self.__getTuples(user_request)
+
         formatted_results = []
-        if result:
+        """ if result:
             for item in result:
                 formatted_string = f"Documento scelto: {item['text']}\nPunteggio di rilevanza: {item['score']:.4f}"
                 formatted_results.append(formatted_string)
             formatted_output = "\n".join(formatted_results)
         else:
-            formatted_output = "Nisba"
+            formatted_output = "Nisba" """
         
-        print(formatted_output)
+        for i in enumerate(relevant_tuples):
+            print(i)
 
 def main():
     manager = IndexManager()
-    documents = [
+
+    """ documents = [
         (0, {"text":"Passeggiata mattutina lungo la spiaggia di sabbia, osservando l'alba", "intent":"descrizione delle attività"}, None),
         (1, {"text":"Costruire castelli di sabbia con i bambini, un'attività divertente per la famiglia", "intent":"attività familiari"}, None),
         (2, {"text":"Assaporare un cocktail di frutta tropicale seduti al bar sulla spiaggia", "intent":"cibo e bevande"}, None),
@@ -85,14 +111,23 @@ def main():
         (6, {"text":"Cenare in riva al mare con vista sulle onde, una serata romantica", "intent":"esperienze culinarie"}, None),
         (7, {"text":"Partecipare a lezioni di surf per imparare a cavalcare le onde", "intent":"apprendimento di sport acquatici"}, None),
         (8, {"text":"Esplorare i fondali marini attraverso immersioni guidate", "intent":"avventure subacquee"}, None),
-        (9, {"text":"Godersi il fresco della sera in una passeggiata lungomare", "intent":"attività serali"}, None)
-    ]
+        (9, {"text":"Godersi il fresco della sera in una passeggiata lungomare", "intent":"attività serali"}, None) 
+    ] """
 
-    manager.createOrLoadIndex(documents)
+    manager.setDataDictName("orders")
 
-    prompt = "bella giornata al mare"
+    manager.loadIndex()
+
+    #manager.createOrLoadIndex(documents)
+
+    """ prompt = "bella giornata al mare"
     result = manager.getResult(prompt)
-    manager.printFormattedResult(result)
+    manager.printFormattedResult(result) """
+    #print(manager.data_dict_name)
+    """ utils_folder_path = os.path.dirname(os.path.realpath(__file__))
+    dictionaries_folder_path = os.path.abspath(os.path.join(utils_folder_path, "Indici")) """
+    #print(dictionaries_folder_path)
+    manager.promptGenerator("give me the email of the user whose name starts with the letter a")
 
 if __name__ == "__main__":
     main()
