0b157cf5ce5bda31307bd3af9d3e0d6672199d6c
stoccotommaso02 <stoccotommaso02@gmail.com>
2024-06-02 15:02:16 +0200

    Update rivisto, aggiunto type hinting

--- App/database_connector.py
+++ App/database_connector.py
@@ -5,29 +5,25 @@ class DatabaseConnector:
     def __init__(self):
         self.connection = streamlit.connection("sqlite", "sql")
 
-    # Ritorna una riga di dataframe ("username", "password")
-    def fetch_user(self, username):
+    def fetch_user(self, username: str):
         result = self.connection.query("SELECT username, password FROM admins WHERE username = :x", params={"x":username})
         return result.iloc[0]
 
-    # Ritorna una riga di dataframe ("id", "name, "description")
-    def __fetch_dictionary_by_id__(self, id):
+    def __fetch_dictionary_by_id__(self, id: int):
         result = self.connection.query("SELECT id, name, description FROM dictionaries WHERE id = :x", params={"x":id})
         return result.iloc[0]
     
-    def __fetch_dictionary_by_name__(self, name):
+    def __fetch_dictionary_by_name__(self, name: str):
         result = self.connection.query("SELECT id, name, description FROM dictionaries WHERE name = :x", params={"x":name})
         return result.iloc[0]
     
-    def fetch_dictionary(self, table_field, value):
+    def fetch_dictionary(self, table_field: str, value: str):
         if(table_field == "id"):
             return self.__fetch_dictionary_by_id__(value)
         if(table_field == "name"):
             return self.__fetch_dictionary_by_name__(value)
 
-
-    # Raises ValueError for null or non unique values
-    def insert_dictionary(self, name, description=None):
+    def insert_dictionary(self, name: str, description: str =None) -> bool:
         if(name == None or name == ""):
             return False
         try:
@@ -38,7 +34,7 @@ class DatabaseConnector:
         except sqlalchemy.exc.IntegrityError:
                 raise ValueError("A dictionary with that name already exists")
     
-    def __delete_dictionary_by_id__(self, id):
+    def __delete_dictionary_by_id__(self, id: int) -> bool:
         if(self.__fetch_dictionary_by_name__(id).empty):
             return False
         with self.connection.session as session:
@@ -46,7 +42,7 @@ class DatabaseConnector:
             session.commit()
         return True
         
-    def __delete_dictionary_by_name__(self, name):
+    def __delete_dictionary_by_name__(self, name: str) -> bool:
         if(self.__fetch_dictionary_by_name__(name).empty):
             return False
         with self.connection.session as session:
@@ -54,38 +50,24 @@ class DatabaseConnector:
             session.commit()
         return True
 
-    def delete_dictionary(self, table_field, field_value):
+    def delete_dictionary(self, table_field: str, field_value: str) -> bool:
         if(table_field == "id"):
             return self.__delete_dictionary_by_id__(field_value)
         if(table_field == "name"):
             return self.__delete_dictionary_by_name__(field_value)
         return False
 
-
-    def __update_dictionary_name__(self, old_name, new_name):
-        if(old_name == None):
+    def update_dictionary(self, key_field: str, key_value, update_field: str, update_value: str) -> bool:
+        validinput =    (key_field == "id" or key_field == "name") and \
+                        (update_field == "name" or update_field == "description") and \
+                        ((update_value != "" and update_value != None) if update_field == "name" else True) # Additional check for an updated name to be not None or empty
+        if(not validinput or self.fetch_dictionary(key_field, key_value).empty): # Previous checks fail or entry does not exist 
             return False
-        if(new_name == None or new_name == ""):
-            return False
+        query = f"UPDATE dictionaries SET {update_field} = :y WHERE {key_field} = :x"
         try:
-            with self.connection.session as session:
-                session.execute(sqlalchemy.text("UPDATE dictionaries SET name = :y WHERE name = :x"),{"x" : old_name, "y" : new_name})
+            with self.connnection.session as session:
+                session.execute(sqlalchemy.text(query), {"x" : key_value, "y" : update_value})
                 session.commit()
             return True
         except sqlalchemy.exc.IntegrityError:
             raise ValueError("A dictionary with that name already exists")
-
-    def __update_dictionary_description__(self, name, new_description):
-        if(self.__fetch_dictionary_by_name__(name).empty):  # A dictionary with that name could not be found
-            return False
-        with self.connection.session as session:
-            session.execute(sqlalchemy.text("UPDATE dictionries SET description = :y WHERE name = :x"), {"x" : name, "y" : new_description})
-            session.commit()
-        return True
-
-    def update_dictionary(self, dictionary_name, table_field, new_value):
-        if(table_field == "name"):
-            return self.__update_dictionary_name__(dictionary_name, new_value)            
-        if(table_field == "description"):
-            return self.__update_dictionary_description__(dictionary_name, new_value)
-        return False
