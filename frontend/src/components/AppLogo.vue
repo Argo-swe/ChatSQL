9a602cdc7765d1a477a9f7b15b48e4dc41d07e8a
Rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2024-08-02 13:32:19 +0100

    aggiunti commenti in stile typedoc

--- frontend/src/components/AppLogo.vue
+++ frontend/src/components/AppLogo.vue
@@ -1,13 +1,14 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AppLogo'
+});
+</script>
+
 <template>
   <img src="../../public/icons/argo_icona.svg" alt="Logo" height="40" class="logo" />
 </template>
-
-<script>
-export default {
-  name: 'AppLogo'
-};
-
-</script>
 
 <style scoped>
 .logo {
--- frontend/src/services/message.service.ts
+++ frontend/src/services/message.service.ts
@@ -6,6 +6,13 @@ const defaultLifeTime = 5000;
 export const messageService = () => {
   const toast = useToast();
 
+  /**
+  * Displays an informational message with optional lifetime.
+  *
+  * @param title - The title or summary of the message.
+  * @param detail - Description of the message.
+  * @param lifeTime - (Optional) The duration in milliseconds for which the message will be displayed. If not provided, a default lifetime will be used.
+  */
   const messageInfo = (title: string, detail: string, lifeTime?: number) => {
     message({
       severity: 'info',
