907d02a52cf30aeb40dbea8204617a87d8b27934
Rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2024-08-03 10:43:41 +0200

    add logo

--- frontend/src/components/AppLogo.vue
+++ frontend/src/components/AppLogo.vue
@@ -1,13 +1,36 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'AppLogo'
+  name: 'AppLogo',
+  props: {
+    path: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: String,
+      default: 'auto'
+    },
+    height: {
+      type: String,
+      default: 'auto'
+    }
+  },
+  setup(props) {
+    const logoSrc = computed(() => {
+      return props.path.startsWith('/') ? process.env.BASE_URL + props.path : props.path;
+    });
+
+    return {
+      logoSrc
+    };
+  }
 });
 </script>
 
 <template>
-  <img src="../../public/icons/argo_icona.svg" alt="Logo" height="40" class="logo" />
+  <img :src="logoSrc" alt="" :width="width" :height="height" class="logo" />
 </template>
 
 <style scoped>
