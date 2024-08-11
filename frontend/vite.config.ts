e9e5e9b4fe9c34cf2a1f26b5faf0d6ec0936b986
rikicavaz <120377343+Rikicavaz77@users.noreply.github.com>
2024-08-11 17:45:32 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,18 @@ import { fileURLToPath, URL } from 'node:url';
 
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
++    istanbul({
++      include: 'src/*',
++      exclude: ['node_modules', 'tests/', 'cypress/'],
++      extension: ['.js', '.ts', '.vue'],
++      requireEnv: false
++    })
++  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
