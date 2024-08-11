a1b0147ee02ef7c6ccf0cf684d88fd15cf30a8f4
rikicavaz <120377343+Rikicavaz77@users.noreply.github.com>
2024-08-11 18:06:41 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
      vue(),
 +    istanbul({
-+      include: 'src/*',
++      include: ["src/**/*", "src/**/*.vue"],
 +      exclude: ['node_modules', 'tests/', 'cypress/'],
 +      extension: ['.js', '.ts', '.vue'],
 +      requireEnv: false
