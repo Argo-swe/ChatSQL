299007e2076f7b2f9f2de689174b3287f1655c4a
Mattia Zecchinato <mattia.zecchinato@studenti.unipd.it>
2024-08-13 13:49:54 +0200

    sistemato tipi ritorno get_preview

--- backend/app/adapter/outcoming/file/json_file_adapter.py
+++ backend/app/adapter/outcoming/file/json_file_adapter.py
@@ -1,5 +1,6 @@
 import os
 import aiofiles
+from models.dictionary_internal_structure.table_dto import TableDto
 from models.dictionary_preview_dto import DictionaryPreviewDto
 from core.port.outcoming.file_repository import FileRepository
 from tools.utils import Utils
@@ -7,7 +8,7 @@ from tools.utils import Utils
 
 class JsonFileAdapter(FileRepository):
 
-    def __init__(self, file_path: str) -> None:
+    def __init__(self, file_path="/opt/chatsql/dictionary_schemas") -> None:
         self._out_file_base_path = file_path
         os.makedirs(self._out_file_base_path, exist_ok=True)
 
@@ -34,7 +35,11 @@ class JsonFileAdapter(FileRepository):
             temp = {"name": table["name"], "description": table["description"]}
             tables.append(temp)
         dictionary_preview["tables"] = tables
-        return DictionaryPreviewDto(dictionary_preview)
+        return DictionaryPreviewDto(
+            database_name=dictionary_preview["database_name"],
+            database_description=dictionary_preview["database_description"],
+            tables=[TableDto(**table) for table in dictionary_preview["tables"]],
+        )
 
     def _generate_schema_file_name(self, id: int) -> str:
         return f"{self._out_file_base_path}/dic_schema_{id}.json"
--- backend/app/routes/prompt_api.py
+++ backend/app/routes/prompt_api.py
@@ -7,12 +7,12 @@ from adapter.outcoming.txtai.txtai_index_manager_adapter import TxtaiIndexManage
 from adapter.outcoming.txtai.txtai_prompt_manager_adapter import (
     TxtaiPromptManagerAdapter,
 )
+from adapter.outcoming.file.json_file_adapter import JsonFileAdapter
 from models.responses.string_data_response_dto import StringDataResponseDto
 from models.responses.prompt_response_dto import PromptResponseDto
 from auth.jwt_bearer import JwtBearer
 from core.service.prompt_manager_service import PromptManagerService
 from core.service.dictionary_service import DictionaryService
-
 
 from fastapi import APIRouter, Depends, Query
 
@@ -21,8 +21,11 @@ router = APIRouter()
 
 # TODO: ottimizzare gli import (Dep inj o singleton?)
 dictionary_repository = SqlAlchemyDictionaryRepositoryAdapter()
+file_repository = JsonFileAdapter()
 index_manager = TxtaiIndexManagerAdapter()
-dictionary_service = DictionaryService(dictionary_repository, index_manager)
+dictionary_service = DictionaryService(
+    dictionary_repository, index_manager, file_repository
+)
 prompt_manager = TxtaiPromptManagerAdapter(index_manager)
 prompt_manager_service = PromptManagerService(dictionary_service, prompt_manager)
 
