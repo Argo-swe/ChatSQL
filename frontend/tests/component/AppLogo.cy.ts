8c0d6fb4cbad22209c4a80ef8cec546c8e27db02
Sebastiano Lewental <151509931+LewentalDigital@users.noreply.github.com>
2024-08-15 11:01:51 +0200

    Test FE AppLogo (#78)
    * creato test AppLogo
    
    * Update frontend/tests/component/AppLogo.cy.ts
    
    Co-authored-by: rikicavaz <120377343+Rikicavaz77@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: rikicavaz <120377343+Rikicavaz77@users.noreply.github.com>

---
+++ frontend/tests/component/AppLogo.cy.ts
@@ -0,0 +1,15 @@
+import AppLogo from '../../src/components/AppLogo.vue'
+
+describe('AppLogo', () => {
+  it('renders correctly', () => {
+    cy.mount(AppLogo, {
+        props: {
+            path: 'icons/argo_trasparente.svg'
+        }
+    })
+    
+    // Assert that the component renders correctly
+    cy.get('img').should('be.visible')
+    cy.get('img').should('have.attr', 'src', 'icons/argo_trasparente.svg')
+  })
+})
