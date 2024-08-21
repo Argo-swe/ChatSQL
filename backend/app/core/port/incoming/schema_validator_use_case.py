85a4d92b9d5fafc8877f618c5efc6c0dbe460d83
Mattia Zecchinato <mattia.zecchinato@studenti.unipd.it>
2024-08-21 10:06:27 +0200

    aggiunta documentazione BE schemaValidatorUseCase

--- backend/app/core/port/incoming/schema_validator_use_case.py
+++ backend/app/core/port/incoming/schema_validator_use_case.py
@@ -5,4 +5,12 @@ class SchemaValidatorUseCase(ABC):
 
     @abstractmethod
     def validate(self, dictionary) -> bool:
+        """Validate the given dictionary schema.
+
+        Args:
+            dictionary (Any): The dictionary schema to be validated.
+
+        Returns:
+            bool: True if the schema is valid, False otherwise.
+        """
         pass
