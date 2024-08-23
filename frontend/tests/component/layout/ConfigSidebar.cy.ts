242cdd1e02b327c6a26b79fc60663a4b45fc0aab
Rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2024-08-23 04:04:01 +0200

    update test config sidebar

--- frontend/tests/component/layout/ConfigSidebar.cy.ts
+++ frontend/tests/component/layout/ConfigSidebar.cy.ts
@@ -1,4 +1,9 @@
+import Button from 'primevue/button';
 import PrimeVue from 'primevue/config';
+import Dropdown from 'primevue/dropdown';
+import InputSwitch from 'primevue/inputswitch';
+import Sidebar from 'primevue/sidebar';
+import { ref } from 'vue';
 import { createI18n } from 'vue-i18n';
 import ConfigSidebar from '../../../src/components/layout/ConfigSidebar.vue';
 
@@ -10,28 +15,22 @@ const i18n = createI18n({
   message: 'Mock translation'
 });
 
-function mountConfigSidebar(props?) {
+function mountConfigSidebar(mockLayoutConfig?, props?) {
   return cy.mount(ConfigSidebar, {
     global: {
       plugins: [i18n, PrimeVue],
       mocks: {
         t: (key) => key,
         layoutState: {
-          configSidebarVisible: {
-            value: true
-          }
+          configSidebarVisible: ref(true)
         },
-        layoutConfig: {
-          scale: {
-            value: 1
-          },
-          theme: {
-            value: 'aura-light-blue'
-          },
-          darkTheme: {
-            value: false
-          }
-        }
+        ...mockLayoutConfig
+      },
+      components: {
+        PgSidebar: Sidebar,
+        PgButton: Button,
+        PgInputSwitch: InputSwitch,
+        PgDropdown: Dropdown
       }
     },
     props
@@ -39,15 +38,82 @@ function mountConfigSidebar(props?) {
 }
 
 describe('ConfigSidebar Component', () => {
-  it('should display correctly', () => {
+  beforeEach(() => {
     mountConfigSidebar();
-
-    cy.get('.layout-config-sidebar').should('be.visible');
   });
 
-  it('should change scale on scale button press', () => {
-    mountConfigSidebar();
+  it('should display correctly', () => {
+    cy.get('[data-testid="config-sidebar"]').should('be.visible');
+    cy.get('[data-testid="manage-scale-section"]').should('be.visible');
+    cy.get('[data-testid="manage-theme-section"]').should('be.visible');
+    cy.get('[data-testid="manage-language-section"]').should('be.visible');
+  });
 
-    cy.get('[data-test-id="increase-scale"]').click();
+  it('should decrease scale', () => {
+    cy.window().then((win) => {
+      const originalFontSize = parseFloat(
+        win.getComputedStyle(win.document.documentElement).fontSize
+      );
+      cy.get('[data-testid="decrease-scale-button"]').click();
+      cy.window().then(() => {
+        const newFontSize = parseFloat(win.getComputedStyle(win.document.documentElement).fontSize);
+        expect(newFontSize).to.be.lessThan(originalFontSize);
+      });
+    });
+  });
+
+  it('should increase scale', () => {
+    cy.window().then((win) => {
+      const originalFontSize = parseFloat(
+        win.getComputedStyle(win.document.documentElement).fontSize
+      );
+      cy.get('[data-testid="increase-scale-button"]').click();
+      cy.window().then(() => {
+        const newFontSize = parseFloat(win.getComputedStyle(win.document.documentElement).fontSize);
+        expect(newFontSize).to.be.greaterThan(originalFontSize);
+      });
+    });
+  });
+
+  it('should disable decrease button', () => {
+    mountConfigSidebar({
+      layoutConfig: {
+        scale: ref(12),
+        theme: ref('aura-light-blue'),
+        darkTheme: ref(false)
+      }
+    });
+    cy.get('[data-testid="decrease-scale-button"]').should('be.disabled');
+  });
+
+  it('should disable increase button', () => {
+    mountConfigSidebar({
+      layoutConfig: {
+        scale: ref(16),
+        theme: ref('aura-light-blue'),
+        darkTheme: ref(false)
+      }
+    });
+    cy.get('[data-testid="increase-scale-button"]').should('be.disabled');
+  });
+
+  it('should toggle dark mode', () => {
+    cy.get('[data-testid=theme-input-switch]').find('input[type="checkbox"]').check();
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem('darkTheme')).to.equal('true');
+    });
+    cy.get('[data-testid=theme-input-switch]').find('input[type="checkbox"]').uncheck();
+    cy.wait(100);
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem('darkTheme')).to.equal('false');
+    });
+  });
+
+  it('should change the language to italian', () => {
+    cy.get('[data-testid="language-dropdown"]').click();
+    cy.contains('locale.it').click({ force: true });
+    cy.window().then((win) => {
+      expect(win.localStorage.getItem('language')).to.equal('it');
+    });
   });
 });
