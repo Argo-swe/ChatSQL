37d546fba78953a6d827c051de3c8f333714e66c
Mattia Zecchinato <mattia.zecchinato@studenti.unipd.it>
2024-08-29 21:50:03 +0200

    rimosso controllo caratteri speciali in descrizione dizionario dati BE

--- backend/app/models/dictionary_dto.py
+++ backend/app/models/dictionary_dto.py
@@ -13,7 +13,7 @@ class DictionaryDto(CamelModel):
 
     valid_pattern: ClassVar[re.Pattern] = re.compile(r"^[\w\s\-]+$")
 
-    @field_validator("name", "description", mode="before")
+    @field_validator("name", mode="before")
     def validate_name_and_description(cls, value: str) -> str:  # noqa: N805
         if not cls.valid_pattern.match(value):
             raise ValueError(
