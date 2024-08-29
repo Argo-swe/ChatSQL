d12e5868a99599a2cd746c8c0355e2ddc258672a
Rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2024-08-29 21:17:08 +0100

    mocks

--- frontend/tests/component/views/AppLayout.cy.ts
+++ frontend/tests/component/views/AppLayout.cy.ts
@@ -1,12 +1,18 @@
 import { ref } from 'vue';
 import { createI18n } from 'vue-i18n';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import DialogService from 'primevue/dialogservice';
+import ToastService from 'primevue/toastservice';
 
 import LoginDialog from '@/components/LoginDialog.vue';
 import AppTopbar from '@/components/layout/AppTopbar.vue';
 import ConfigSidebar from '@/components/layout/ConfigSidebar.vue';
 import MenuSidebar from '@/components/layout/MenuSidebar.vue';
 
-import AppLayout from '../../../src/views/AppLayout.vue';
+import AppLayout from '@/views/AppLayout.vue';
 
 const i18n = createI18n({
   legacy: false,
@@ -16,22 +22,30 @@ const i18n = createI18n({
   message: 'Mock translation'
 });
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: { template: '<div>Home</div>' } },
+    { path: '/test', component: { template: '<div>Test</div>' } }
+  ]
+});
+
 function mountAppLayout(props?) {
   return cy.mount(AppLayout, {
     global: {
-      plugins: [i18n],
+      plugins: [i18n, PrimeVue, ConfirmationService, DialogService, ToastService, router],
       mocks: {
         t: (key) => key,
-        layoutState: {
-          configSidebarVisible: ref(true)
-        }
       },
       components: {
         LoginDialog,
         AppTopbar,
         ConfigSidebar,
         MenuSidebar
-      }
+      },
+      /* stubs: {
+        AppMenu: true
+      } */
     },
     props
   });
@@ -40,7 +54,6 @@ function mountAppLayout(props?) {
 describe('AppLayout Component', () => {
   it('should render correctly', () => {
     mountAppLayout();
-    cy.get('[data-testid="app-layout"]').should('be.visible');
-    
+    cy.get('[data-testid="layout-wrapper"]').should('be.visible');
   });
 });
