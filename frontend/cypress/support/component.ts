f0c32feefedde41ea5de626cf94e9e290e8c4a57
rikicavaz <120377343+Rikicavaz77@users.noreply.github.com>
2024-09-01 16:55:16 +0100

    Update component.ts

--- frontend/cypress/support/component.ts
+++ frontend/cypress/support/component.ts
@@ -19,6 +19,7 @@ import './commands';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
+import '@cypress/code-coverage/support';
 import { mount } from 'cypress/vue';
 
 // Augment the Cypress namespace to include type definitions for
