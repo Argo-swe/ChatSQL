5572084f2d43fdd22576f23dfd9fa2c48b67a747
rikicavaz <120377343+Rikicavaz77@users.noreply.github.com>
2024-09-01 16:52:56 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     vue(),
     istanbul({
       include: 'src/*',
-      exclude: ['node_modules', 'tests/', 'cypress/'],
+      exclude: ['node_modules', 'src/composables/status-messages.ts', 'src/services/api-client.service.ts', 'src/services/message.service.ts', 'tests/', 'cypress/'],
       extension: ['.js', '.ts', '.vue'],
     })
   ],
