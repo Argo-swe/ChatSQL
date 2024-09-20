45a996878fcfe68d3af74ede019d99d49b8f01f0
Rikicavaz77 <riccardo.cavalli@studenti.unipd.it>
2024-09-20 18:08:05 +0200

    update tipi di ritorno

--- backend/app/adapter/outcoming/db_manager/sql_alchemy/sql_alchemy_dictionary_repository_adapter.py
+++ backend/app/adapter/outcoming/db_manager/sql_alchemy/sql_alchemy_dictionary_repository_adapter.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from models.dictionary_dto import DictionaryDto
 from .models import Dictionaries
 from .base import SessionLocal
@@ -11,33 +11,57 @@ class SqlAlchemyDictionaryRepositoryAdapter(DictionaryRepository):
         self._session = session
 
     def get_all_dictionaries(self) -> List[DictionaryDto]:
-        return self._session.query(Dictionaries).all()
+        dictionaries = self._session.query(Dictionaries).all()
 
-    def get_dictionary_by_id(self, id: int) -> DictionaryDto:
-        return self._session.query(Dictionaries).filter(Dictionaries.id == id).first()
+        return [DictionaryDto.from_orm(dictionary) for dictionary in dictionaries]
 
-    def get_dictionary_by_name(self, name: str) -> DictionaryDto:
-        return (
+    def get_dictionary_by_id(self, id: int) -> Optional[DictionaryDto]:
+        dictionary = (
+            self._session.query(Dictionaries).filter(Dictionaries.id == id).first()
+        )
+
+        if dictionary is None:
+            return None
+
+        return DictionaryDto.from_orm(dictionary)
+
+    def get_dictionary_by_name(self, name: str) -> Optional[DictionaryDto]:
+        dictionary = (
             self._session.query(Dictionaries).filter(Dictionaries.name == name).first()
         )
+
+        if dictionary is None:
+            return None
+
+        return DictionaryDto.from_orm(dictionary)
 
     def create_dictionary(self, name: str, description: str) -> DictionaryDto:
         db_dictionary = Dictionaries(name=name, description=description)
         self._session.add(db_dictionary)
         self._session.commit()
         self._session.refresh(db_dictionary)
-        return db_dictionary
+        return DictionaryDto.from_orm(db_dictionary)
 
-    def update_dictionary(self, id: int, name: str, description: str) -> DictionaryDto:
-        current_dictionary = self.get_dictionary_by_id(id)
+    def update_dictionary(
+        self, id: int, name: str, description: str
+    ) -> Optional[DictionaryDto]:
+        current_dictionary = (
+            self._session.query(Dictionaries).filter(Dictionaries.id == id).first()
+        )
+
+        if current_dictionary is None:
+            return None
+
         current_dictionary.name = name
         current_dictionary.description = description
         self._session.commit()
         self._session.refresh(current_dictionary)
-        return current_dictionary
+        return DictionaryDto.from_orm(current_dictionary)
 
     def delete_dictionary(self, id: int):
-        dictionary = self.get_dictionary_by_id(id)
+        dictionary = (
+            self._session.query(Dictionaries).filter(Dictionaries.id == id).first()
+        )
         if dictionary:
             self._session.delete(dictionary)
             self._session.commit()
